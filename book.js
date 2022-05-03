63f631751647417cedc6319ebfb4c7c94eb99a8b
labstraction <45163263+labstraction@users.noreply.github.com>
2022-05-03 07:32:01 +0200

    class descriptions

---
+++ book.js
@@ -0,0 +1,17 @@
+///book
+
+///properties:
+///title
+///author
+///editor
+///type
+///price
+///copies
+///pages
+///yop
+///discount
+
+///methods:
+///toString()
+///getPublicPrice() => price + 10% - discount + 30%
+
---
+++ magazine.js
@@ -0,0 +1,15 @@
+///magazine
+
+///properties:
+///title
+///editor
+///release
+///cadence
+///type
+///price
+///copies
+///discount
+
+///methods:
+///toString()
+///getPublicPrice() => price + 20% - discount + 30%
